2d1f041db171ade811f5f55771585a50f84203ba
DellLaptop <17853728804@163.com>
2017-12-06 13:15:53 +0800

    CRUD

--- src/com/xin/mybatis/dao/IUser.java
+++ src/com/xin/mybatis/dao/IUser.java
@@ -1,8 +1,16 @@
 package com.xin.mybatis.dao;
+import java.util.List;
+
 import org.apache.ibatis.annotations.*;
+
 import com.xin.mybatis.models.User;
 
 public interface IUser {
-	@Select("select * from user where id = #{id}")
-	public User getUserById(int id);
+	//@Select("select * from user where id = #{id}")
+	//public User getUserById(int id);
+	public void insertUser(User user);
+	public List<User> getUserList();
+	public void deleteUser(int id);
+	public void updateUser(User user);
+	public User getUser(int id);
 }
--- src/com/xin/mybatis/main/Main.java
+++ src/com/xin/mybatis/main/Main.java
@@ -1,6 +1,8 @@
 package com.xin.mybatis.main;
 
 import java.io.Reader;
+import java.text.MessageFormat;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -29,13 +31,87 @@ public class Main {
 	public static void main(String[] args){
 		SqlSession session=sqlSessionFactory.openSession();
 		try{
-			IUser iuser = session.getMapper(IUser.class);
-			User user =iuser.getUserById(1);
-			System.out.println("名字:"+user.getName());
-			System.out.println("所属部门:"+user.getDept());
-			System.out.println("主页:"+user.getWebsite());
+			//testInsert();
+			//getUserList();
+			//deleteUser();
+			updateUser();
 		}finally {
 			session.close();
+		}
+	}
+	private static void updateUser() {
+		// TODO Auto-generated method stub
+		try{
+			SqlSession session = sqlSessionFactory.openSession();
+			IUser iuser = session.getMapper(IUser.class);
+			System.out.println("Test update start ...");
+			printUsers(iuser.getUserList());
+			User user = iuser.getUser(1);
+			user.setName("New Name");
+			iuser.updateUser(user);
+			session.commit();
+			System.out.println("After update");
+			printUsers(iuser.getUserList());
+			System.out.println("Test update finished ...");
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	private static void deleteUser() {
+		// TODO Auto-generated method stub
+		try{
+			SqlSession session = sqlSessionFactory.openSession();
+			IUser iuser = session.getMapper(IUser.class);
+			System.out.println("Test delete start ...");
+			System.out.println("Before delete");
+			printUsers(iuser.getUserList());
+			iuser.deleteUser(2);
+			session.commit();
+			System.out.println("After delete");
+			printUsers(iuser.getUserList());
+			System.out.println("Test delete finished ...");
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	public static void testInsert(){
+		try{
+			SqlSession session = sqlSessionFactory.openSession();
+			IUser userMapper = session.getMapper(IUser.class);
+			System.out.println("Test insert start...");
+			User user = new User();
+			user.setId(0);
+			user.setName("Google");
+			user.setDept("Tech");
+			user.setWebsite("http:www.google.com");
+			user.setPhone("17853728804");
+			userMapper.insertUser(user);
+			session.commit();
+		}catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public static void getUserList(){
+		try{
+			SqlSession session = sqlSessionFactory.openSession();
+			IUser iuser = session.getMapper(IUser.class);
+			System.out.println("Test Get start");
+			printUsers(iuser.getUserList());
+			System.out.println("Test Get finish");
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	private static void printUsers(final List<User> users) {
+		// TODO Auto-generated method stub
+		int count = 0;
+		for (User user: users){
+			System.out.println(MessageFormat.format("++++++++++", ++count));
+			System.out.println("User Id:"+ user.getId());
+			System.out.println("User Name:"+ user.getName());
+			System.out.println("User Dept:"+ user.getDept());
+			System.out.println("User Website:"+ user.getWebsite());
 		}
 	}
 }
